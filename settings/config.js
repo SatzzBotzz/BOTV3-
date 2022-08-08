f7b4965a04c7c0a9ed552f63fd3c5668ea8c985f
SatganzDevs <101557898+SatzzBotzz@users.noreply.github.com>
2022-08-08 22:03:01 +0700

    Satganz Devs

--- settings/config.js
+++ settings/config.js
@@ -11,45 +11,14 @@ global.APIKeys = {
 'https://zenzapis.xyz': 'satganzdevs',
 }
 
-//gw benci apikey, maka dari itu gwejh bikin ini.
-global.gambar =[
-"https://telegra.ph/file/74a73f50e97b2c29a1915.jpg",
-"https://telegra.ph/file/a4250101c9059351b4c2a.jpg",
-"https://telegra.ph/file/edcde3b17573c0b4ad8b1.jpg",
-"https://telegra.ph/file/be50ada7f42556ecf9b62.jpg",
-"https://telegra.ph/file/207c47dc216620473867f.jpg",
-"https://telegra.ph/file/d4b79d46fde189a209e26.jpg",
-"https://telegra.ph/file/33b46f8df0fe399909ec5.jpg",
-"https://telegra.ph/file/74e4a4985333f2200cd13.jpg",
-"https://telegra.ph/file/a3091ce936d62ea76777f.jpg",
-"https://telegra.ph/file/a4d8267969c17fbda65b2.jpg",
-"https://telegra.ph/file/592cdbee867e1071b3470.jpg",
-"https://telegra.ph/file/52cff7d080473d41e4f31.jpg",
-"https://telegra.ph/file/73030742eebd3a081beac.jpg",
-"https://telegra.ph/file/adf8660e77ee40ad3afc6.jpg",
-"https://telegra.ph/file/6dcf0f3aacefec6ebcf1a.jpg",
-"https://telegra.ph/file/6260249b574a96b70c529.jpg",
-"https://telegra.ph/file/6b1cf58706f891603d311.jpg",
-"https://telegra.ph/file/e6c0aa8328cc20275fac1.jpg",
-"https://telegra.ph/file/965d1d547b813b9c3caa0.jpg",
-"https://telegra.ph/file/20275fad1c2550d620c42.jpg",
-"https://telegra.ph/file/d4b79d46fde189a209e26.jpg",
-"https://telegra.ph/file/e3b245089226158cfe311.jpg",
-"https://telegra.ph/file/73fea62cd1ecfa36ef000.jpg",
-"https://telegra.ph/file/86964a140cd68d898a474.jpg",
-"https://telegra.ph/file/b20f6ebaffb8a5ee3ee50.jpg",
-"https://telegra.ph/file/105175e45624e34c5d2b6.jpg",
-"https://telegra.ph/file/56cd28524bc2cc7bb27a6.jpg",
-"https://telegra.ph/file/ed674135fed56a1b88290.jpg"]
-
 // Other
-global.web = 'https://s.satganzdevs.repl.co'
+global.web = 'https://bit.ly/SatganzDevs'
 global.thumb = { url : "https://zenzapis.xyz/randomimage/kucing?apikey=satganzdevs" }
 global.owner = ['6281316701742']
 global.premium = ["6281266891985","6281266082412","6282182518286","6282232780320","6281369266141"]
-global.botname = 'Sad Bots'
+global.botname = '𝘈𝘵𝘵𝘢𝘤𝘬𝘦𝘳𝘴 𝘉𝘰𝘵𝘴'
 global.ownername = '𝗦𝗮𝘁𝗴𝗮𝗻𝘇 𝗗𝗲𝘃𝘀'
-global.packname = 'Sad Bot'
+global.packname = '𝘈𝘵𝘵𝘢𝘤𝘬𝘦𝘳𝘴 𝘉𝘰𝘵𝘴'
 global.author = 'Satganz Devs'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
@@ -63,13 +32,13 @@ tmalam: 'Good Midnight'
 }
 global.mess = {
 success: '✓ Success',
-admin: 'Group Admin Special Features! o(´^｀)o',
-botAdmin: 'Bots Should Be Admins First! ¯_ಠ_ಠ_/¯ ',
-owner: 'Special owner bot features! (✖╭╮✖)',
-group: 'Feature Used For Group Only! (º_º) ',
+admin: 'Group Admin Special Features! ',
+botAdmin: 'Bots Should Be Admins First! ',
+owner: 'Special owner bot features! ',
+group: 'Feature Used For Group Only! ',
 premium: 'Special Premium Users Features',
-private: 'Features Used Only For Private Chat（Ω_Ω）! ',
-bot: 'Bot Number User Special Features ( ・_・)ノ ',
+private: 'Features Used Only For Private Chat ',
+bot: 'Bot Number User Special Features ',
 wait: 'Loading...',
 endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours ',
 }
@@ -77,7 +46,10 @@ global.limitawal = {
 premium: "Infinity",
 free: 10
 }
-global.visoka = 'https://a.uguu.se/QorFgDAm.mp4'
+global.visok = ['https://telegra.ph/file/067b2cb3312837533239c.mp4',
+'https://telegra.ph/file/e38881701692c74484a17.mp4',
+'https://telegra.ph/file/de776d34ef058b7d2ec12.mp4', 'https://telegra.ph/file/bc82653506c301b40679c.mp4',     'https://telegra.ph/file/7f10b3624991bbcee9ded.mp4', 'https://telegra.ph/file/51aa9701839dcc29066e9.mp4',     'https://telegra.ph/file/4f26132ac0296a34a45a8.mp4']
+global.visoka = visok[Math.floor(Math.random() * visok.length)]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
